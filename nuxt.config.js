e064d2a1d192d145f5a94619bafb5ae4630f4547
Abayomi Ayodele <samabayomi301@gmail.com>
2021-11-03 12:30:00 +0100

    movies grid

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,8 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+
+    // '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <AppHero />
+  <main>
+    <AppHero />
+
+    <AppMoviesGrid :movies="movies" />
+  </main>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
+  async fetch() {
+    await this.getMovies();
+  },
+
+  methods: {
+    async getMovies() {
+      const data = await axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en&page=1`
+      );
+
+      const result = await data;
+
+      this.movies = result.data.results;
+    },
+  },
+};
 </script>
