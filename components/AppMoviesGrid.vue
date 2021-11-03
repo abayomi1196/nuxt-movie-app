971acc699a0db7f28ba5ae67275fb943f070dcd4
Abayomi Ayodele <samabayomi301@gmail.com>
2021-11-03 13:31:52 +0100

    implement searchbar

--- components/AppMoviesGrid.vue
+++ components/AppMoviesGrid.vue
@@ -1,6 +1,49 @@
 <template>
   <div class="container movies">
-    <div id="movie-grid" class="movies-grid">
+    <!-- searched movies -->
+    <div v-if="searchInput.length" id="movie-grid" class="movies-grid">
+      <div v-for="movie in searchedMovies" :key="movie.id" class="movie">
+        <div class="movie-img">
+          <img
+            :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+            :alt="movie.title"
+          />
+
+          <p class="review">
+            {{ movie.vote_average }}
+          </p>
+          <p class="overview">
+            {{ movie.overview }}
+          </p>
+        </div>
+
+        <div class="info">
+          <p class="title">
+            {{ movie.title.slice(0, 25) }}
+            <span v-if="movie.title.length > 25">...</span>
+          </p>
+
+          <p class="release">
+            Released:
+            {{
+              new Date(movie.release_date).toLocaleString('en-gb', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric',
+              })
+            }}
+          </p>
+
+          <nuxt-link class="button button-light" :to="`movies/${movie.id}`">
+            More Details
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+    <!-- end of searched movies -->
+
+    <!-- streaming movies -->
+    <div v-else id="movie-grid" class="movies-grid">
       <div v-for="movie in movies" :key="movie.id" class="movie">
         <div class="movie-img">
           <img
@@ -39,6 +82,7 @@
         </div>
       </div>
     </div>
+    <!-- end of streaming movies -->
   </div>
 </template>
 
@@ -49,6 +93,14 @@ export default {
     movies: {
       type: Array,
       default: () => [],
+    },
+    searchedMovies: {
+      type: Array,
+      default: () => [],
+    },
+    searchInput: {
+      type: String,
+      default: '',
     },
   },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,26 @@
   <main>
     <AppHero />
 
-    <AppMoviesGrid :movies="movies" />
+    <!-- searchBar -->
+    <div class="container search">
+      <input
+        v-model.lazy="searchTerm"
+        type="text"
+        placeholder="Search Nuxt Movie App.."
+        @keyup.enter="$fetch"
+      />
+
+      <button v-show="searchTerm !== ''" class="button" @click="clearSearch">
+        Clear Search
+      </button>
+    </div>
+    <!-- end of searchBar -->
+
+    <AppMoviesGrid
+      :movies="movies"
+      :searched-movies="searchedMovies"
+      :search-input="searchTerm"
+    />
   </main>
 </template>
 
@@ -13,23 +32,71 @@ export default {
   data() {
     return {
       movies: [],
+      searchedMovies: [],
+      searchTerm: '',
     };
   },
 
   async fetch() {
-    await this.getMovies();
+    if (this.searchTerm === '') {
+      await this.getMovies();
+      return;
+    }
+
+    if (this.searchTerm !== '') {
+      await this.searchMovies();
+    }
   },
 
   methods: {
     async getMovies() {
       const data = await axios.get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en&page=1`
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=7d21bbfa3334a3920e67d193b9fbd3a2&language=en-US&page=1`
       );
 
       const result = await data;
 
       this.movies = result.data.results;
     },
+
+    async searchMovies() {
+      const data = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?api_key=7d21bbfa3334a3920e67d193b9fbd3a2&language=en-US&page=1&query=${this.searchTerm}&include_adult=true`
+      );
+
+      const result = await data;
+
+      this.searchedMovies = result.data.results;
+    },
+
+    clearSearch() {
+      this.searchTerm = '';
+      this.searchedMovies = [];
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.search {
+  display: flex;
+  padding: 32px 16px;
+
+  input {
+    max-width: 350px;
+    width: 100%;
+    padding: 12px 6px;
+    font-size: 14px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
+</style>
